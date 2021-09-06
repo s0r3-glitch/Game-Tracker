0c81525254eeb4e34261381094a112b9d4630455
s0r3-glitch <ggoodson2432@gmail.com>
2021-09-05 23:54:06 -0400

    Updated the main.py file to write data from the steam web api to the mongoDB I think

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import art
 from colorama import Fore, init
 import pymongo
 import requests
+import json
 
 # initalized colorama
 init()
@@ -39,18 +40,24 @@ DBname = config.ReadConfig('DBinfo', 'dbname')
 print(DBname)
 
 # Connecting to the MongoDO and specifying the DB we are going to use
-# print('beep')
-# myclient = pymongo.MongoClient(DBaddress)
-# mydb = myclient[DBname]
-# mycol = mydb['Games']
-# print(mydb.list_collection_names())
-
-# mydict = { "name": "John", "address": "Highway 37" }
-# x = mycolSteam.insert_one(mydict)
-# print('boop')
-# x = mycolSteam.find_one()
-# print(x)
-
+print('beep')
+myclient = pymongo.MongoClient(DBaddress)
+mydb = myclient[DBname]
+mycol = mydb['Games']
+print(mydb.list_collection_names())
 steamresponse = requests.get(f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={WebAPIKey}&steamid={SteamID}&include_appinfo=1")
-print(steamresponse.text)
-print(type(steamresponse.text))
+y = json.loads(steamresponse.text)
+print(y["response"]["game_count"])
+for i in range(y["response"]["game_count"]):
+    query = { "appid": y['response']['games'][i]['appid'] }
+    mydoc = mycol.find(query)
+    for x in mydoc:
+        print(x)
+    if mydb.list_collection_names() == []:
+        entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
+        x = mycol.insert_one(entrie)
+    elif mydoc == '':
+        entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
+        x = mycol.insert_one(entrie)
+    else:
+        print('Game match sorry')
