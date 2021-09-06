32315fd08503336edf9fc5321f0787e059ea73f7
s0r3-glitch <ggoodson2432@gmail.com>
2021-09-06 00:41:29 -0400

    Added a way to manually add games. Added a menu to the main function. Completed the most of the basic functionality that goes with the core of the project.

--- main.py
+++ main.py
@@ -40,24 +40,68 @@ DBname = config.ReadConfig('DBinfo', 'dbname')
 print(DBname)
 
 # Connecting to the MongoDO and specifying the DB we are going to use
-print('beep')
-myclient = pymongo.MongoClient(DBaddress)
-mydb = myclient[DBname]
-mycol = mydb['Games']
-print(mydb.list_collection_names())
-steamresponse = requests.get(f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={WebAPIKey}&steamid={SteamID}&include_appinfo=1")
-y = json.loads(steamresponse.text)
-print(y["response"]["game_count"])
-for i in range(y["response"]["game_count"]):
-    query = { "appid": y['response']['games'][i]['appid'] }
-    mydoc = mycol.find(query)
-    if not mydb.list_collection_names():
-        print('1')
-        entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
+def Steamautoadd():
+    #print('beep')
+    myclient = pymongo.MongoClient(DBaddress)
+    mydb = myclient[DBname]
+    mycol = mydb['Games']
+    #print(mydb.list_collection_names())
+    steamresponse = requests.get(f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={WebAPIKey}&steamid={SteamID}&include_appinfo=1")
+    y = json.loads(steamresponse.text)
+    #print(y["response"]["game_count"])
+    newlyadded = 0
+    skipped = 0
+    for i in range(y["response"]["game_count"]):
+        query = { "appid": y['response']['games'][i]['appid'] }
+        mydoc = mycol.find(query)
+        if not mydb.list_collection_names():
+            #print('1')
+            entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
+            x = mycol.insert_one(entrie)
+            newlyadded += 1 
+        elif mydoc.count() == 0:
+            #print(2)
+            entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
+            x = mycol.insert_one(entrie)
+            newlyadded =+ 1
+        else:
+            #print('Game exists in DB')
+            skipped += 1
+    print(Fore.GREEN + f'Steam library update complete:\nNewly added game: {newlyadded}\nGames skipped: {skipped}')
+
+def ManualAdd():
+    myclient = pymongo.MongoClient(DBaddress)
+    mydb = myclient[DBname]
+    mycol = mydb['Games']
+    loop = True
+    while loop is True:
+        clear()
+        name = str(input('Name of the game:'))
+        name = name.title()
+        platform = str(input('Platform the game is on:'))
+        platform = platform.title()
+        type = str(input('Is the game physical or digital:'))
+        type = type.title()
+        if type == 'Digital':
+            location = 'Digital'
+        else:
+            location = str(input('Where is the game now:'))
+        entrie = {"name": name, "platform": platform, "Type": type, "Location": location}
         x = mycol.insert_one(entrie)
-    elif mydoc.count() == 0:
-        print(2)
-        entrie = {"name": y['response']['games'][i]['name'], "platform": "Steam", "Type": "Digital", "Location": "Digital", "img_icon": y['response']['games'][i]['img_icon_url'], "img_logo": y['response']['games'][i]['img_logo_url'], "appid": y['response']['games'][i]['appid']} 
-        x = mycol.insert_one(entrie)
-    else:
-        print('Game exists in DB')
+        temp = str(input('Would you like to add more game (yes or no):'))
+        temp = temp.title()
+        if temp == 'No':
+            loop = False
+
+def main():
+    loop = True
+    while loop is True:
+        choice = int(input('What would you like to do today?\n1. Update your Steam Libreary\n2. Manually add a game\n3. Exit\n'))
+        if choice == 1:
+            Steamautoadd()
+        elif choice == 2:
+            ManualAdd()
+        elif choice == 3:
+            exit(0)
+
+main()
